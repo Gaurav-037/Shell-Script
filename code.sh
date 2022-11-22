62f8881b8559f256be0cff716d20c0d709e44b7d
Gaurav-037 <85051029+Gaurav-037@users.noreply.github.com>
2022-11-22 23:05:14 +0530

    if elif else

--- code.sh
+++ code.sh
@@ -8,3 +8,34 @@ do
     echo "$i";
 done 
 
+# Given two integers, X and Y, find their sum, difference, product, and quotient
+read X
+read Y
+sum=$(( $X + $Y ))
+sub=$(( $X - $Y ))
+mul=$(( $X * $Y ))
+div=$(( $X / $Y ))
+echo "$sum"
+echo "$sub"
+echo "$mul"
+echo "$div"
+
+#Given two integers, X and Y, identify whether X>Y or X==Y  orX<Y .
+#Exactly one of the following lines:
+#- X is less than Y
+#- X is greater than Y
+#- X is equal to Y
+read X
+read Y
+if [ $X -gt $Y ] 
+then 
+echo "X is greater than Y"
+elif [ $X == $Y ]
+then
+echo "X is equal to Y"
+elif [ $X -lt $Y ]
+then
+echo "X is less than Y"
+else
+echo " "
+fi
