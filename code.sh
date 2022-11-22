af5c7652c5afe06eb64fde1a3edcb918f816dddf
Gaurav-037 <85051029+Gaurav-037@users.noreply.github.com>
2022-11-22 22:31:26 +0530

    for loop

--- code.sh
+++ code.sh
@@ -1,3 +1,10 @@
 #basic code of taking input and display name
 read name;
 echo "Welcome $name";
+
+#Use a for loop to display the natural numbers from 1 to 50
+for i in {1..50..1}
+do
+    echo "$i";
+done 
+
