40daa8df8537f648ec7dd17fac1c2ffae79d42e9
Gaurav-037 <85051029+Gaurav-037@users.noreply.github.com>
2022-11-22 23:33:55 +0530

    if elif else using three variables

--- code.sh
+++ code.sh
@@ -39,3 +39,26 @@ echo "X is less than Y"
 else
 echo " "
 fi
+
+#Given three integers (X,Y , and Z ) representing the three sides of a triangle, identify whether the triangle is scalene, isosceles, or equilateral.
+
+#If all three sides are equal, output EQUILATERAL.
+#Otherwise, if any two sides are equal, output ISOSCELES.
+#Otherwise, output SCALENE.
+
+read X
+read Y
+read Z
+
+if [ $X == $Y ] && [ $X == $Z ] && [ $Y == $Z ]
+then
+echo "EQUILATERAL"
+elif [ $X == $Y ] || [ $X == $Z ] || [ $Y == $Z ]
+then
+echo "ISOSCELES"
+elif [ $X != $Y ] || [ $X != $Z ]
+then
+echo "SCALENE"
+else
+echo ""
+fi
